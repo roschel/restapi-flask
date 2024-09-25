a5432992c88fffc36ef4496557d463ad8ce51a09
roschel <joaomroschel@gmail.com>
2024-09-25 09:56:54 -0300

    fix: requirements and wsgi PORT

--- wsgi.py
+++ wsgi.py
@@ -7,4 +7,4 @@ else:
     app = create_app('config.ProdConfig')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", debug=True, port=os.getenv("PORT", 5000))
